5c8a4f1e9fe206e05024e49e5e419d5e98129294
gangagyatso4364 <gyatsokhan@gmail.com>
2023-12-15 14:26:29 +0530

    added test for func count_non_bo_words

--- tests/test_text_analyzer.py
+++ tests/test_text_analyzer.py
@@ -8,3 +8,14 @@ def test_count_non_words():
     tokens = ta.tokenize_text(text)
     actual_non_word_count = ta.count_non_words(tokens)
     assert expected_non_word_count == actual_non_word_count, "Non-word count mismatch"
+
+
+def test_count_non_bo_words():
+    text = "abdul kalamའཁྱེད་ལ་སུན་པོ་ཀཟོས་པར་དགོངས་དག་ཞུ་ཨུམ་ཨུམ་་་། ()$%322 ༣༢༢་�� 你好吗 कैसे ཨོཾ་མ་ཎི་པདྨེ་ཧཱུྃ"
+    expected_non_bo_word_count = 5
+    ta = TextAnalyzer(0.2)
+    tokens = ta.tokenize_text(text)
+    actual_non_bo_word_count = ta.count_non_bo_words(tokens)
+    assert (
+        expected_non_bo_word_count == actual_non_bo_word_count
+    ), "Non-Bo word count mismatch"
