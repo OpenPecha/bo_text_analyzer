414584cbc436132497164238a58ab5b4c9f452bd
gangagyatso4364 <gyatsokhan@gmail.com>
2023-12-15 14:23:36 +0530

    added test for func count_non_words

---
+++ tests/test_text_analyzer.py
@@ -0,0 +1,10 @@
+from bo_text_analyzer.text_analyzer import TextAnalyzer
+
+
+def test_count_non_words():
+    text = "abdul kalamའཁྱེད་ལ་སུན་པོ་ཀཟོས་པར་དགོངས་དག་ཞུ་ཨུམ་ཨུམ་་་། ()$%322 ༣༢༢་�� 你好吗 कैसे ཨོཾ་མ་ཎི་པདྨེ་ཧཱུྃ"
+    expected_non_word_count = 4
+    ta = TextAnalyzer(0.2)
+    tokens = ta.tokenize_text(text)
+    actual_non_word_count = ta.count_non_words(tokens)
+    assert expected_non_word_count == actual_non_word_count, "Non-word count mismatch"
