e83fa315db86d424e0494a3631c5c30f08f8906c
gangagyatso4364 <gyatsokhan@gmail.com>
2023-12-15 13:16:07 +0530

    update func count_non_words

--- src/bo_text_analyzer/text_analyzer.py
+++ src/bo_text_analyzer/text_analyzer.py
@@ -1,13 +1,15 @@
 from pathlib import Path
+
 from botok import WordTokenizer
 from botok.config import Config
+
 
 class TextAnalyzer:
     def __init__(self, preminum_threshold):
         self.text = {}
         self.text_report = {}
         self.preminum_threshold = preminum_threshold
-    
+
     def get_text(self):
         pass
 
@@ -15,18 +17,24 @@ class TextAnalyzer:
         wt = WordTokenizer(config=Config(dialect_name="general", base_path=Path.home()))
         tokens = wt.tokenize(text, split_affixes=False)
         return tokens
-    
+
     def count_non_words(self, tokens):
-        pass
+        non_word_count = 0
+        for token in tokens:
+            if token.pos == "NON_WORD" and not token.skrt:
+                non_word_count += 1
+        return non_word_count
 
     def count_non_bo_words(self, tokens):
         pass
 
     def check_is_preminum(self, non_word_percentage, non_bo_word_percentage):
-        if non_word_percentage > self.preminum_threshold or non_bo_word_percentage > self.preminum_threshold:
+        if (
+            non_word_percentage > self.preminum_threshold
+            or non_bo_word_percentage > self.preminum_threshold
+        ):
             return False
         return True
-
 
     def analyze(self):
         self.text = self.get_text()
@@ -38,12 +46,14 @@ class TextAnalyzer:
             total_non_bo_words = self.count_non_bo_words(tokens)
             non_word_percentage = total_non_words / total_words
             non_bo_word_percentage = total_non_bo_words / total_words
-            is_premium = self.check_is_preminum(non_word_percentage, non_bo_word_percentage)
+            is_premium = self.check_is_preminum(
+                non_word_percentage, non_bo_word_percentage
+            )
 
-            cur_file_report['total_words'] = total_words
-            cur_file_report['total_non_words'] = total_non_words
-            cur_file_report['total_non_bo_words'] = total_non_bo_words
-            cur_file_report['non_word_percentage'] = non_word_percentage
-            cur_file_report['non_bo_word_percentage'] = non_bo_word_percentage
-            cur_file_report['is_premium'] = is_premium
+            cur_file_report["total_words"] = total_words
+            cur_file_report["total_non_words"] = total_non_words
+            cur_file_report["total_non_bo_words"] = total_non_bo_words
+            cur_file_report["non_word_percentage"] = non_word_percentage
+            cur_file_report["non_bo_word_percentage"] = non_bo_word_percentage
+            cur_file_report["is_premium"] = is_premium
             self.text_report[text_file_name] = cur_file_report
