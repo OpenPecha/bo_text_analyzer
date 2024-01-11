e2eec0770e9a41a243576fb610ee4f0211ee11a9
gangagyatso4364 <gyatsokhan@gmail.com>
2024-01-11 14:37:28 +0530

    changed test analyzer

--- tests/test_text_analyzer.py
+++ tests/test_text_analyzer.py
@@ -4,7 +4,7 @@ from bo_text_analyzer.text_analyzer import TextAnalyzer
 def test_count_non_words():
     text = "abdul kalamའཁྱེད་ལ་སུན་པོ་ཀཟོས་པར་དགོངས་དག་ཞུ་ཨུམ་ཨུམ་་་། ()$%322 ༣༢༢་�� 你好吗 कैसे ཨོཾ་མ་ཎི་པདྨེ་ཧཱུྃ"
     expected_non_word_count = 4
-    ta = TextAnalyzer(0.2)
+    ta = TextAnalyzer(non_word_threshold=0.05, no_bo_word_threshold=0.03)
     tokens = ta.tokenize_text(text)
     actual_non_word_count = ta.count_non_words(tokens)
     assert expected_non_word_count == actual_non_word_count, "Non-word count mismatch"
@@ -13,7 +13,7 @@ def test_count_non_words():
 def test_count_non_bo_words():
     text = "abdul kalamའཁྱེད་ལ་སུན་པོ་ཀཟོས་པར་དགོངས་དག་ཞུ་ཨུམ་ཨུམ་་་། ()$%322 ༣༢༢་�� 你好吗 कैसे ཨོཾ་མ་ཎི་པདྨེ་ཧཱུྃ"
     expected_non_bo_word_count = 5
-    ta = TextAnalyzer(0.2)
+    ta = TextAnalyzer(non_word_threshold=0.05, no_bo_word_threshold=0.03)
     tokens = ta.tokenize_text(text)
     actual_non_bo_word_count = ta.count_non_bo_words(tokens)
     assert (
